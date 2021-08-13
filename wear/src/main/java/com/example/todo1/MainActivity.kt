b67ce3e601a4c76dd27a5467f32c17c7141a7679
Pedro Batista <pedrovhb@gmail.com>
2021-08-13 16:50:54 -0300

    Navigation works

--- wear/src/main/java/com/example/todo1/MainActivity.kt
+++ wear/src/main/java/com/example/todo1/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.todo1
 
 
-
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -15,18 +11,21 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.wear.compose.foundation.ArcPaddingValues
 import androidx.wear.compose.foundation.BasicCurvedText
 import androidx.wear.compose.foundation.CurvedRow
+import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ListHeader
 import androidx.wear.compose.material.SplitToggleChip
 import androidx.wear.compose.material.Text
@@ -37,26 +36,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel: TodoListViewModel = viewModel()
-            Todo1Theme {
-                TodoListScreen(
-                    todoList = viewModel.todoList,
-                    onTodoToggled = { viewModel.onTodoToggled(it) })
-            }
-        }
-    }
-}
-
-class TodoListFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                Text(text = "Hello world.")
-            }
+            MyApp()
         }
     }
 }
@@ -64,29 +44,73 @@ class TodoListFragment : Fragment() {
 
 data class Todo(var text: String, var checked: Boolean = false)
 
-val myTodoList = listOf(
-    Todo("Espaço"),
-    Todo("Espaço"),
-    Todo("Café - Ferver água"),
-    Todo("Comer"),
-    Todo("Remédio"),
-    Todo("Café - Desligar água"),
-    Todo("Escovar os dentes"),
-    Todo("Pegar toalha"),
-    Todo("Separar roupas"),
-    Todo("Sabonete rosto"),
-    Todo("Óleo"),
-    Todo("Vestir"),
-    Todo("Estender toalha"),
-    Todo("Espaço"),
-    Todo("Espaço"),
-)
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    val viewModel: TodoListViewModel = viewModel()
+
+    fun goToTodoList(): Unit {
+        println("navController.navigate(\"todoList\")")
+        navController.navigate("todoList")
+    }
+
+    fun goToReversedTodoList(): Unit {
+        println("navController.navigate(\"reversedTodoList\")")
+        navController.navigate("reversedTodoList")
+    }
+
+    Todo1Theme {
+        NavHost(navController = navController, startDestination = "todoList") {
+            composable("todoList") {
+                TodoListScreen(
+                    switchList = { goToReversedTodoList() },
+                    todoList = viewModel.todoList,
+                    onTodoToggled = { viewModel.onTodoToggled(it) })
+            }
+            composable("reversedTodoList") {
+                ReversedTodoListScreen(
+                    switchList = { goToTodoList() },
+                    todoList = viewModel.todoList,
+                    onTodoToggled = { viewModel.onTodoToggled(it) })
+            }
+        }
+    }
+
+}
 
 
 @Composable
-fun TodoListScreen(todoList: List<Todo>, onTodoToggled: (Todo) -> Unit) {
+fun TodoListScreen(
+    switchList: () -> Unit,
+    todoList: List<Todo>,
+    onTodoToggled: (Todo) -> Unit
+) {
     Surface {
-        TodoList(todoList, onTodoToggled)
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = switchList) {
+                Text("Ya!")
+            }
+            TodoList(todoList = todoList, onTodoToggled = onTodoToggled)
+        }
+    }
+}
+
+@Composable
+fun ReversedTodoListScreen(
+    switchList: () -> Unit,
+    todoList: List<Todo>,
+    onTodoToggled: (Todo) -> Unit
+) {
+    Surface {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = switchList) {
+                Text("Yo!")
+            }
+            TodoList(
+                todoList = todoList.reversed(),
+                onTodoToggled = onTodoToggled
+            )
+        }
     }
 }
 
@@ -95,7 +119,7 @@ fun TodoList(todoList: List<Todo>, onTodoToggled: (Todo) -> Unit) {
 
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
+    ) {
 
         item { Spacer(modifier = Modifier.size(20.dp)) }
         item { ListHeader { Text("Header1") } }
@@ -124,7 +148,7 @@ fun TodoListItem(todo: Todo, onTodoToggled: (Todo) -> Unit) {
 @Composable
 fun DefaultPreview() {
     Todo1Theme {
-        TodoListScreen(myTodoList) { }
+        TodoListScreen({}, myTodoList) { }
     }
 }
 
--- wear/src/main/java/com/example/todo1/TodoListViewModel.kt
+++ wear/src/main/java/com/example/todo1/TodoListViewModel.kt
@@ -38,3 +38,21 @@ class TodoListViewModel : ViewModel() {
 //    }
 
 }
+
+val myTodoList = listOf(
+    Todo("Espaço"),
+    Todo("Espaço"),
+    Todo("Café - Ferver água"),
+    Todo("Comer"),
+    Todo("Remédio"),
+    Todo("Café - Desligar água"),
+    Todo("Escovar os dentes"),
+    Todo("Pegar toalha"),
+    Todo("Separar roupas"),
+    Todo("Sabonete rosto"),
+    Todo("Óleo"),
+    Todo("Vestir"),
+    Todo("Estender toalha"),
+    Todo("Espaço"),
+    Todo("Espaço"),
+)
